61c9b97a2c12a56b4a8d653e3b8438d98fb70055
Ильнур Баширов <ilnur.bashirovilnur@yandex.ru>
2022-06-20 23:05:19 +0400

    Добавьте файлы проекта.

---
+++ w8/Pages/AdminServicePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using w8.DateBase;
+
+namespace w8.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminServicePage.xaml
+    /// </summary>
+    public partial class AdminServicePage : Page
+    {
+        public AdminServicePage(User user)
+        {
+            InitializeComponent();
+            tbFIO.Text = user.UserFN + " " + user.UserLN + " " + user.UserMN;
+            UpdateData();
+        }
+
+        public void UpdateData()
+        {
+            IEnumerable<Product> products = EFModel.Init().Products.Where(w => w.ProductName.Contains(tbSearch.Text) || w.ProductCategory.Contains(tbSearch.Text));
+            LvService.ItemsSource = products.ToList();
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ w8/Pages/AuthPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using w8.DateBase;
+
+namespace w8.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btUserClick(object sender, RoutedEventArgs e)
+        {
+            User user = EFModel.Init().Users.FirstOrDefault(u => u.UserLogin == tbLogin.Text && u.UserPassword == tbPass.Text);
+            if(user == null)
+            {
+                MessageBox.Show("Неверный пароль или логин!");
+                return;
+            }
+
+            if(user.Role.RoleName == "Администраторр")
+            {
+                NavigationService.Navigate(new AdminServicePage( user));
+            }
+        }
+
+        private void btAdminClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
